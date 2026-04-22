31d15c6edabb87de241806a6b2cb6af9ce339718
Samuka1507 <samuel.ferreira.mendonca@escola.pr.gov.br>
2026-04-22 12:23:04 -0300

    Add files via upload

--- cinema_absolute/app.py
+++ cinema_absolute/app.py
@@ -122,4 +122,5 @@ def logout():
 
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True)
+    # O host '0.0.0.0' expõe o app na rede local
+    app.run(host='0.0.0.0', port=5000, debug=True)
